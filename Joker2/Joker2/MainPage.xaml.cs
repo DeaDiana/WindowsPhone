1c9de9bc33b39ff48888e4d77ce0a59831daf6c2
DianaAnisutina <freilina_07@mail.ru>
2013-04-19 13:34:01 +0700

    added voice control into the app
    using voice you can choose joke or tongue-twister

--- Joker2/Joker2/MainPage.xaml.cs
+++ Joker2/Joker2/MainPage.xaml.cs
@@ -19,6 +19,11 @@ namespace Joker2
     public partial class MainPage : PhoneApplicationPage
     {
         public const string VoiceCommandFile = @"JokerCommands.xml";
+        Dictionary<string, string> jokeCollection;
+        Dictionary<string, string> tongueTwisterCollection;
+        bool isRecoEnabled = false;
+        IAsyncOperation<SpeechRecognitionResult> recoOperation;
+        SpeechRecognizer recognizer;
 
         public MainPage()
         {
@@ -48,6 +53,27 @@ namespace Joker2
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            if (jokeCollection == null)
+            {
+                jokeCollection = new Dictionary<string, string>();
+                jokeCollection.Add("one", "joke number 1");
+                jokeCollection.Add("two", "joke number 2");
+                jokeCollection.Add("three", "What do you do if a blond throws a grenade at you? Pull the pin and throw it back.");
+            }
+            if (jokeCollection == null)
+            {
+                tongueTwisterCollection = new Dictionary<string, string>();
+                tongueTwisterCollection.Add("one", "twister number 1");
+                tongueTwisterCollection.Add("two", "twister number 2");
+                tongueTwisterCollection.Add("three", "Socks on Knox And Knox in box. Fox in socks On box on Knox.");
+            }
+
+            if (recognizer == null)
+                {
+                    recognizer = new SpeechRecognizer();
+                    recognizer.Grammars.AddGrammarFromUri("CommandsGrammar", new Uri("ms-appx:///SRGSGrammar1.xml", UriKind.Absolute));
+                }
+
             string voiceCommand;
             if (this.NavigationContext.QueryString != null && this.NavigationContext.QueryString.ContainsKey("voiceCommandName"))
             {
@@ -100,14 +126,92 @@ namespace Joker2
 
         private async void RecognizeCommand()
         {
+            if (this.isRecoEnabled)
+            {
+                isRecoEnabled = false;
+                btnStartRecognition.Content = "I wanna fun!";
+                if (recoOperation != null && recoOperation.Status == AsyncStatus.Started)
+                {
+                    recoOperation.Cancel();
+                }
+                return;
+            }
+            else
+            {
+                isRecoEnabled = true;
+                btnStartRecognition.Content = "Stop joking!";
+            }
+
+
             using (SpeechRecognizerUI recognizerUI = new SpeechRecognizerUI())
             {
-                recognizerUI.Settings.ListenText = "Listening the command...";
+               /* recognizerUI.Settings.ListenText = "Listening the command...";
                 recognizerUI.Settings.ExampleText = "\"Next\"";
                 recognizerUI.Settings.ReadoutEnabled = false;
                 recognizerUI.Settings.ShowConfirmation = false;
 
                 recognizerUI.Recognizer.Grammars.AddGrammarFromUri("CommandsGrammar", new Uri("ms-appx:///SRGSGrammar1.xml", UriKind.Absolute));
+                */
+                while (this.isRecoEnabled)
+                {
+                    try
+                    {
+                        recoOperation = recognizer.RecognizeAsync();
+                        var recoResult = await this.recoOperation;
+
+                        if (recoResult.TextConfidence < SpeechRecognitionConfidence.Medium)
+                        {
+                            //await speaker.SpeakTextAsync("Ha-ha-ha. Say it louder!");
+                        }
+                        else
+                        {
+                            SemanticProperty genre;
+
+                            if (recoResult.Semantics.TryGetValue("genre", out genre))
+                            {
+                                string filePath = string.Empty;
+                                object arg = null;
+                                string displayFormat = string.Empty;
+
+                                switch (genre.Value.ToString())
+                                {
+                                    case "joke":
+                                        filePath = "JokeTemplate.xml";
+                                        arg = "smth from joke-list"; //smth from joke-list
+                                        break;
+                                    case "tongue-twister":
+                                        filePath = "TongueTwisterTemplate.xml";
+                                        arg = "smth from tng-tw-list"; //smth from tng-tw-list
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                ReproduceSpeech(filePath, arg);
+                            }
+
+                        }
+                    }
+                    catch (System.Threading.Tasks.TaskCanceledException) { }
+                    catch (Exception err)
+                    {
+                        // Handle the speech privacy policy error.
+                        const int privacyPolicyHResult = unchecked((int)0x80045509);
+                        if (err.HResult == privacyPolicyHResult)
+                        {
+                            MessageBox.Show("To run the app, you must first accept the speech privacy policy. To do so, navigate to Settings -> speech on your phone and check 'Enable Speech Recognition Service' ");
+                            isRecoEnabled = false;
+                            btnStartRecognition.Content = "Start speech recognition";
+                        }
+                        else
+                        {
+                            //textOfJoke.Text = "Error: " + err.Message;
+                        }
+                    }
+                }
+                
+                
+                /*
                 SpeechRecognitionUIResult result = await recognizerUI.RecognizeWithUIAsync();
                 if (result.RecognitionResult.TextConfidence == SpeechRecognitionConfidence.Rejected)
                 {
@@ -140,7 +244,7 @@ namespace Joker2
                     }
 
                     ReproduceSpeech(filePath, arg);
-                }
+                }*/
             }
         }
     }
